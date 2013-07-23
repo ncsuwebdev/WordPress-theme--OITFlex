84c1a888a82ae819ef05f6d92fdb5ee2a8a2fac1
ncsumarit <jen_riehle@ncsu.edu>
2013-07-23 10:45:44 -0400

    more IE fix testing

--- header-oneCol.php
+++ header-oneCol.php
@@ -23,8 +23,8 @@
 <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <title><?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
