9e8801bb3ee8897eae6db498fd5d9ea0e837c6c7
ncsumarit <jen_riehle@ncsu.edu>
2013-07-23 10:32:06 -0400

    testing IE compatability fix

--- header-oneCol.php
+++ header-oneCol.php
@@ -23,6 +23,7 @@
 <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width" />
 <title><?php
 	/*
